24f61170113b148eb8118e80d9d20850087a58f6
Azer <racozer@DESKTOP-RG5A9QI.localdomain>
2022-11-18 23:50:28 +0400

    Added ApiRunner

---
+++ src/platfom.api/Core/Platform.Api.Core.ApplicationRunner/ApiRunner.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+
+namespace Platform.Api.Core.ApplicationRunner;
+
+public class ApiRunner
+{
+    public static void RunApi<TStartup>() where TStartup : class
+    {
+        var app = WebHost.CreateDefaultBuilder().UseStartup<TStartup>().Build();
+
+        app.Run();
+    }
+}
